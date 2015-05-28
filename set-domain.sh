149b7bdbb9a93afcaa57842e8619b40435b8fd25
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2015-05-28 12:46:26 +0200

    Global flag on sed

--- set-domain.sh
+++ set-domain.sh
@@ -13,8 +13,8 @@ fi
 OLD_URL=$(grep 'url.*http' /srv/www/ghost/config.js | cut -d \' -f 2 | head -n 2 | tail -n 1)
 OLD_DOMAIN=$(echo $OLD_URL | cut -d / -f 3)
 
-sed -i "s|$OLD_DOMAIN|$DOMAIN|" $GHOST_CONFIG
-sed -i "s|server_name.*|server_name $DOMAIN;|" $NGINX_CONFIG
+sed -i "s|$OLD_DOMAIN|$DOMAIN|g" $GHOST_CONFIG
+sed -i "s|server_name.*|server_name $DOMAIN;|g" $NGINX_CONFIG
 
 echo "DOMAIN=${DOMAIN}" > data/MAIL
 
